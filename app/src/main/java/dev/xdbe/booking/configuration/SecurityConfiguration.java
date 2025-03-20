7c4d2ba8499d89ff33f60613aa0c20d36d5db31b
Paul FERREIRA <paul.ferreira@epita.fr>
2025-03-20 12:08:55 +0100

    fix: add CSRF

--- app/src/main/java/dev/xdbe/booking/configuration/SecurityConfiguration.java
+++ app/src/main/java/dev/xdbe/booking/configuration/SecurityConfiguration.java
@@ -8,7 +8,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-
+import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
