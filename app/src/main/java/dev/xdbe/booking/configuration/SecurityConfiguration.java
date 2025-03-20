056697c818f429012aa3b844631631ec2aa300d0
Paul FERREIRA <paul.ferreira@epita.fr>
2025-03-20 12:25:20 +0100

    feat: add login form

--- app/src/main/java/dev/xdbe/booking/configuration/SecurityConfiguration.java
+++ app/src/main/java/dev/xdbe/booking/configuration/SecurityConfiguration.java
@@ -18,6 +18,17 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.session.web.http.CookieSerializer;
 import org.springframework.session.web.http.DefaultCookieSerializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
@@ -28,10 +39,9 @@ public class SecurityConfiguration {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
             .authorizeHttpRequests(auth -> auth
-                // Step 3: add authorization
-                .anyRequest().permitAll()
-            )
-            // Step 3: Add login form
+                    .requestMatchers("/dashboard").hasRole("ADMIN")
+                    .anyRequest().permitAll()
+            ).formLogin(withDefaults())
             .csrf((csrf) -> csrf
                 .ignoringRequestMatchers("/h2-console/*")
             )
@@ -39,6 +49,27 @@ public class SecurityConfiguration {
             .build();
     }
 
-    // Step 3: add InMemoryUserDetailsManager
+    @Bean
+    public UserDetailsService users(PasswordEncoder encoder) {
+        User.UserBuilder users = User.builder();
 
+        var admin = users
+                .username("admin")
+                .password(encoder.encode("admin"))
+                .roles("ADMIN")
+                .build();
+
+        var user = users
+                .username("user")
+                .password(encoder.encode("user"))
+                .roles("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(admin, user);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
